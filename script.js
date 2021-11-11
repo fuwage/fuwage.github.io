50f5432bc283f0e4642b3b3418ffb404b4adfddb
fuwage <94106371+fuwage@users.noreply.github.com>
2021-11-11 18:07:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 "use strict";
 
 console.log('test');
+
+const baseImage = [
+    '1.png', '2.png', '3.png', '4.png'
+];
+
+const onakaImage = [
+    '1.png', '2.png', '3.png', '4.png'
+];
+
+let baseChoice = '1.png';
+const baseDiv = document.getElementById('base');
+for (const imgName of baseImage) {
+    const img = document.createElement('img');
+    img.src = 'image/' + imgName;
+    img.size = '50';
+    baseDiv.appendChild(img);
+    img.addEventListener('click', () => {
+        baseChoice = imgName;
+    });
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    console.log(baseChoice);
+});
+
